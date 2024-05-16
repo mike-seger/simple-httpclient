9e5db97ba5712a2820f4ce335e214e5ddb2626b7
Michael Seger <michael@net128.com>
2024-05-16 13:09:43 +0200

    ...

--- src/main/java/com/example/demo/RestClientConfig.java
+++ src/main/java/com/example/demo/RestClientConfig.java
@@ -1,14 +1,14 @@
 package com.example.demo;
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.DefaultHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,15 +39,19 @@ public class RestClientConfig {
 
     @Bean
     public RestTemplate restTemplate() throws Exception {
-        SSLContext sslContext = createSslContext(); // Correct SSL context creation
+        SSLContext sslContext = createSslContext();
 
-        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, new DefaultHostnameVerifier());
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                sslContext, new DefaultHostnameVerifier());
+
         Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
                 .register("https", sslsf)
                 .register("http", new PlainConnectionSocketFactory())
                 .build();
 
-        BasicHttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(socketFactoryRegistry);
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+        connectionManager.setMaxTotal(200); // Set max total connections
+        connectionManager.setDefaultMaxPerRoute(20); // Set max connections per route
 
         CloseableHttpClient httpClient = HttpClients.custom()
                 .setConnectionManager(connectionManager)
@@ -56,13 +60,13 @@ public class RestClientConfig {
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
         return new RestTemplate(requestFactory);
     }
+
     private SSLContext createSslContext() throws Exception {
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
         try (var keyStoreInputStream = this.keyStore.getInputStream()) {
             keyStore.load(keyStoreInputStream, keyStorePassword.toCharArray());
         }
 
-        // Load the trust store
         KeyStore trustStore = KeyStore.getInstance("PKCS12");
         try (var trustStoreInputStream = this.trustStore.getInputStream()) {
             trustStore.load(trustStoreInputStream, trustStorePassword.toCharArray());
@@ -79,5 +83,4 @@ public class RestClientConfig {
 
         return sslContext;
     }
-
 }
